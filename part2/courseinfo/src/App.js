41bb817e10e970e0d0e79773e4949f3a6ba9df55
prajitlal4 <prajit.lal4@gmail.com>
2022-07-31 17:59:33 +0800

    2.1 - created Course component

---
+++ part2/courseinfo/src/App.js
@@ -0,0 +1,47 @@
+const Course = ({course}) => {
+  return (
+    <div>
+      <h1>{course.name}</h1>
+      <p>
+        {course.parts.map(part=>
+          <p key={part.id}>
+            {part.name} {part.exercises}
+          </p>
+        )}
+      </p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      },
+      {
+        name: 'Programming languages (TEST)',
+        exercises: 9,
+        id: 4
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
