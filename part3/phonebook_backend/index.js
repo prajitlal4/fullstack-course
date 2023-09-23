0fa62ff970f485031f95c66a2e3b2609875abdf7
prajitlal4 <prajit.lal4@gmail.com>
2023-09-23 15:06:47 +0800

    get person ID

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -39,6 +39,20 @@ app.get('/info', (request, response) => {
   response.send(string)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
+    response.json(person)
+  }
+  else {
+    response.status(404).json({
+      error: 'No Person found with that ID'
+    })
+  }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
